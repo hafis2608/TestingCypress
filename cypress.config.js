4aa8d4aca98a89969e1d101e4e26b7373591049c
Aloysius Riandika <riandikahermawan@gmail.com>
2025-02-24 23:28:54 +0700

    verify login failed and verify login succes-homepage-myaccount

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com',
+    watchForFileChanges: false, // Disable auto reload on file changes
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/edit_account-information.cy.js
+++ cypress/e2e/edit_account-information.cy.js
@@ -1,5 +1,26 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+//TC Homepage -> Login Page
+describe('Verify Login Failed', () => {
+//TC Login Page -> Login Failed
+  it("Login Failed - Email and Password Not Registered", () => {
+    cy.visit("https://magento.softwaretestingboard.com/");
+    cy.contains("Sign In").click()
+    cy.get('#email').type("cobaaku@mail.com")
+    cy.get('#pass').type("Coba123")
+    cy.get('#send2').click()
+    cy.get('.message-error > div').should("be.visible")  
+  });
+});
+describe('Verify Login Success to My Account', () => {
+//TC Login Page -> Homepage - My Account page
+  it("Homepage - My Account Information", () => {
+    cy.visit("https://magento.softwaretestingboard.com/")
+    cy.contains("Sign In").click()
+    cy.get('#email').type("akucoba@gmail.com")
+    cy.get('#pass').type("Qwerty123")
+    cy.get('#send2').click()
+    cy.get('.logged-in').should("be.visible")
+    cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+    cy.contains("My Account").click()
+    cy.contains("Account Information").should("be.visible")    
+  });
+});
