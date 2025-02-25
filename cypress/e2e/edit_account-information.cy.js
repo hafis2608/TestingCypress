47348f3e2429e19c206b4782ac150575c48a23b2
Aloysius Riandika <riandikahermawan@gmail.com>
2025-02-25 18:47:17 +0700

    add test case from login to edit account information page

--- cypress/e2e/edit_account-information.cy.js
+++ cypress/e2e/edit_account-information.cy.js
@@ -1,17 +1,18 @@
 //TC Homepage -> Login Page
 describe('Verify Login Failed', () => {
-//TC Login Page -> Login Failed
-  it("Login Failed - Email and Password Not Registered", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
-    cy.contains("Sign In").click()
-    cy.get('#email').type("cobaaku@mail.com")
-    cy.get('#pass').type("Coba123")
-    cy.get('#send2').click()
-    cy.get('.message-error > div').should("be.visible")  
+  //TC Login Page -> Login Failed
+    it("Login Failed - Email and Password Not Registered", () => {
+      cy.visit("https://magento.softwaretestingboard.com/");
+      cy.contains("Sign In").click()
+      cy.get('#email').type("cobaaku@mail.com")
+      cy.get('#pass').type("Coba123")
+      cy.get('#send2').click()
+      cy.get('.message-error > div').should("be.visible")
+    });
   });
-});
-describe('Verify Login Success to My Account', () => {
+
 //TC Login Page -> Homepage - My Account page
+describe('Verify Login Success to My Account', () => {
   it("Homepage - My Account Information", () => {
     cy.visit("https://magento.softwaretestingboard.com/")
     cy.contains("Sign In").click()
@@ -21,6 +22,28 @@ describe('Verify Login Success to My Account', () => {
     cy.get('.logged-in').should("be.visible")
     cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
     cy.contains("My Account").click()
-    cy.contains("Account Information").should("be.visible")    
+    cy.get('.block-dashboard-info > .block-title > strong').should('have.text', 'Account Information')
+    cy.get('.block-dashboard-info > .block-content > .box > .box-title > span').should('have.text', 'Contact Information')
+    cy.get('.box-title').should('be.visible')
+    cy.get('.box-content').should('be.visible')
+    cy.get('.box-content > p').should('contain.text','gmail.com')
+    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+    cy.get('.change-password').should('be.visible')  });
+});
+describe('Verify Login Success to Edit Account', () => {
+//TC Login Page -> Homepage - My Account page -> Edit Account Information Page
+  it("Homepage - My Account Information - Edit Account Information", () => {
+    cy.visit("https://magento.softwaretestingboard.com/")
+    cy.contains("Sign In").click()
+    cy.get('#email').type("akucoba@gmail.com")
+    cy.get('#pass').type("Qwerty123")
+    cy.get('#send2').click()
+    cy.get('.logged-in').should("be.visible")
+    cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+    cy.contains("My Account").click()
+    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+    cy.get('.change-password').should('be.visible')
+    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+    cy.url().should('include', '/customer/account/edit')
   });
 });
