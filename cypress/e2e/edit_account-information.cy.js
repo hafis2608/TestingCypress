da3dd1741fadcff0afa22bf0f9d88d0bd5611947
Aloysius Riandika <riandikahermawan@gmail.com>
2025-02-26 01:30:55 +0700

    final TC edit_account

--- cypress/e2e/edit_account-information.cy.js
+++ cypress/e2e/edit_account-information.cy.js
@@ -1,28 +1,143 @@
-describe('Verify Login Success to Edit Account', () => {
-//TC Login Page -> Homepage - My Account page -> Edit Account Information Page
-  it("Homepage - My Account Information - Edit Account Information", () => {
-    cy.visit("https://magento.softwaretestingboard.com/")
-    cy.contains("Sign In").click()
-    cy.get('#email').type("akucoba@gmail.com")
-    cy.get('#pass').type("Qwerty123")
-    cy.get('#send2').click()
-    cy.get('.logged-in').should("be.visible")
-    cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
-    cy.contains("My Account").click()
-    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
-    cy.get('.change-password').should('be.visible')
-    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
-    cy.url().should('include', '/customer/account/edit')
-    cy.get('#change-email').check()
-    cy.get('#email').clear()
-    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-    cy.get('#email-error').should('contain.text','This is a required field.')
-    cy.get('#email').type("cobadulu@gmail.com")
-    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-    cy.get('#current-password-error').should('contain.text','This is a required field.')
-    cy.get('#current-password').type("Qwerty123")
-    cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
-    cy.get('.message-success').should('be.visible')
-    cy.get('.message-success').should('contain.text','You saved the account information.')
-  });
-});
+describe('Verify Login Failed', () => {
+    //TC Login Page -> Login Failed
+      it("Login Failed - Email and Password Not Registered", () => {
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Sign In").click()
+        cy.get('#email').type("cobaaku@mail.com")
+        cy.get('#pass').type("Coba123")
+        cy.get('#send2').click()
+        cy.get('.message-error > div').should("be.visible")
+        cy.get('.message-error > div').should("contain.text", 'The account sign-in was incorrect or your account is disabled temporarily.')
+      });
+    });
+describe('Verify Login to Edit Account Success', () => {
+    //TC Login Page -> Homepage - My Account page -> Edit Account Information Page
+      it("Can access Edit Account Information", () => {
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Sign In").click()
+        cy.get('#email').type("akucoba@gmail.com")
+        cy.get('#pass').type("Qwerty123")
+        cy.get('#send2').click()
+        cy.get('.logged-in').should("be.visible")
+        cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+        cy.contains("My Account").click()
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+        cy.get('.change-password').should('be.visible')
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+        cy.url().should('include', '/customer/account/edit')
+      });
+    });
+describe('Verify Change Email Failed', () => {
+    //TC Change Email Failed - current password false
+      it("Can't change Email in Edit Account Information - Current Password False", () => {
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Sign In").click()
+        cy.get('#email').type("akucoba@gmail.com")
+        cy.get('#pass').type("Qwerty123")
+        cy.get('#send2').click()
+        cy.get('.logged-in').should("be.visible")
+        cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+        cy.contains("My Account").click()
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+        cy.get('.change-password').should('be.visible')
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+        cy.url().should('include', '/customer/account/edit')
+        cy.get('#change-email').check()
+        cy.get('#email').clear()
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('#email-error').should('contain.text','This is a required field.')
+        cy.get('#email').type("cobadulu@gmail.com")
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('#current-password-error').should('contain.text','This is a required field.')
+        cy.get('#current-password').type("qwerty123")
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('.message-error').should('be.visible')
+        cy.get('.message-error').should('contain.text','Verify the password and try again.')
+      });
+    });
+
+describe('Verify Change Email Success', () => {
+    //TC Change email Success - current password true
+      it("Can change Email in Edit Account Information", () => {
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Sign In").click()
+        cy.get('#email').type("akucoba@gmail.com")
+        cy.get('#pass').type("Qwerty123")
+        cy.get('#send2').click()
+        cy.get('.logged-in').should("be.visible")
+        cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+        cy.contains("My Account").click()
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+        cy.get('.change-password').should('be.visible')
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+        cy.url().should('include', '/customer/account/edit')
+        cy.get('#change-email').check()
+        cy.get('#email').clear()
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('#email-error').should('contain.text','This is a required field.')
+        cy.get('#email').type("cobadulu@gmail.com")
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('#current-password-error').should('contain.text','This is a required field.')
+        cy.get('#current-password').type("Qwerty123")
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('.message-success').should('be.visible')
+        cy.get('.message-success').should('contain.text','You saved the account information.')
+      });
+    });
+  describe('Verify Change Password Failed', () => {
+    //TC Change Password Failed - current password false
+      it("Can't change Password in Edit Account Information - Current Password False", () => {
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Sign In").click()
+        cy.get('#email').type("cobadulu@gmail.com")
+        cy.get('#pass').type("Qwerty123")
+        cy.get('#send2').click()
+        cy.get('.logged-in').should("be.visible")
+        cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+        cy.contains("My Account").click()
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+        cy.get('.change-password').should('be.visible')
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+        cy.url().should('include', '/customer/account/edit')
+        cy.get('#change-password').check()
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('#current-password-error').should('contain.text','This is a required field.')
+        cy.get('#password-error').should('contain.text','This is a required field.')
+        cy.get('#password-confirmation-error').should('contain.text','This is a required field.')
+        cy.get('#current-password').type("qwerty123")
+        cy.get('#password').type("Qawsed123")
+        cy.get('#password-confirmation').type("Qawsed123")
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('.message-error').should('be.visible')
+        cy.get('.message-error').should('contain.text','Verify the password and try again.')
+      });
+    });
+
+  describe('Verify Change Password Success', () => {
+    //TC Change Password Success - current password true
+      it("Can change Password in Edit Account Information", () => {
+        cy.visit("https://magento.softwaretestingboard.com/")
+        cy.contains("Sign In").click()
+        cy.get('#email').type("cobadulu@gmail.com")
+        cy.get('#pass').type("Qwerty123")
+        cy.get('#send2').click()
+        cy.get('.logged-in').should("be.visible")
+        cy.get(':nth-child(2) > .customer-welcome > .customer-name > .action').click()
+        cy.contains("My Account").click()
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').should('be.visible')
+        cy.get('.change-password').should('be.visible')
+        cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
+        cy.url().should('include', '/customer/account/edit')
+        cy.get('#change-password').check()
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('#current-password-error').should('contain.text','This is a required field.')
+        cy.get('#password-error').should('contain.text','This is a required field.')
+        cy.get('#password-confirmation-error').should('contain.text','This is a required field.')
+        cy.get('#current-password').type("Qwerty123")
+        cy.get('#password').type("Qawsed123")
+        cy.get('#password-confirmation').type("Qawsed123")
+        cy.get('#form-validate > .actions-toolbar > div.primary > .action').click()
+        cy.get('.message-success').should('be.visible')
+        cy.get('.message-success').should('contain.text','You saved the account information.')
+      });
+    });
