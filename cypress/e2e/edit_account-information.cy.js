3aa098af8e2eaeb25d7e0511a69ded9d4da75161
Aloysius Riandika <riandikahermawan@gmail.com>
2025-02-26 11:15:31 +0700

    Final - just add beforeEach

--- cypress/e2e/edit_account-information.cy.js
+++ cypress/e2e/edit_account-information.cy.js
@@ -1,7 +1,9 @@
 describe('Verify Login Failed', () => {
-    //TC Login Page -> Login Failed
+      beforeEach(() => {
+        cy.visit('https://magento.softwaretestingboard.com/')
+      })  
+  //TC Login Page -> Login Failed
       it("Login Failed - Email and Password Not Registered", () => {
-        cy.visit("https://magento.softwaretestingboard.com/")
         cy.contains("Sign In").click()
         cy.get('#email').type("cobaaku@mail.com")
         cy.get('#pass').type("Coba123")
@@ -9,11 +11,8 @@ describe('Verify Login Failed', () => {
         cy.get('.message-error > div').should("be.visible")
         cy.get('.message-error > div').should("contain.text", 'The account sign-in was incorrect or your account is disabled temporarily.')
       });
-    });
-describe('Verify Login to Edit Account Success', () => {
     //TC Login Page -> Homepage - My Account page -> Edit Account Information Page
       it("Can access Edit Account Information", () => {
-        cy.visit("https://magento.softwaretestingboard.com/")
         cy.contains("Sign In").click()
         cy.get('#email').type("akucoba@gmail.com")
         cy.get('#pass').type("Qwerty123")
@@ -26,11 +25,8 @@ describe('Verify Login to Edit Account Success', () => {
         cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click()
         cy.url().should('include', '/customer/account/edit')
       });
-    });
-describe('Verify Change Email Failed', () => {
     //TC Change Email Failed - current password false
       it("Can't change Email in Edit Account Information - Current Password False", () => {
-        cy.visit("https://magento.softwaretestingboard.com/")
         cy.contains("Sign In").click()
         cy.get('#email').type("akucoba@gmail.com")
         cy.get('#pass').type("Qwerty123")
@@ -54,12 +50,9 @@ describe('Verify Change Email Failed', () => {
         cy.get('.message-error').should('be.visible')
         cy.get('.message-error').should('contain.text','Verify the password and try again.')
       });
-    });
 
-describe('Verify Change Email Success', () => {
     //TC Change email Success - current password true
       it("Can change Email in Edit Account Information", () => {
-        cy.visit("https://magento.softwaretestingboard.com/")
         cy.contains("Sign In").click()
         cy.get('#email').type("akucoba@gmail.com")
         cy.get('#pass').type("Qwerty123")
@@ -83,11 +76,8 @@ describe('Verify Change Email Success', () => {
         cy.get('.message-success').should('be.visible')
         cy.get('.message-success').should('contain.text','You saved the account information.')
       });
-    });
-  describe('Verify Change Password Failed', () => {
     //TC Change Password Failed - current password false
       it("Can't change Password in Edit Account Information - Current Password False", () => {
-        cy.visit("https://magento.softwaretestingboard.com/")
         cy.contains("Sign In").click()
         cy.get('#email').type("cobadulu@gmail.com")
         cy.get('#pass').type("Qwerty123")
@@ -111,12 +101,8 @@ describe('Verify Change Email Success', () => {
         cy.get('.message-error').should('be.visible')
         cy.get('.message-error').should('contain.text','Verify the password and try again.')
       });
-    });
-
-  describe('Verify Change Password Success', () => {
     //TC Change Password Success - current password true
       it("Can change Password in Edit Account Information", () => {
-        cy.visit("https://magento.softwaretestingboard.com/")
         cy.contains("Sign In").click()
         cy.get('#email').type("cobadulu@gmail.com")
         cy.get('#pass').type("Qwerty123")
